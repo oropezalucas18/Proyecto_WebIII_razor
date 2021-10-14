b53dc4e3f8b58d33db6afa400d0b3e99355b2fe8
AleUlloa2504 <65233048+AleUlloa2504@users.noreply.github.com>
2021-10-14 16:48:46 -0400

    Insert update y delete en comentarios y login registro en Person

---
+++ Proyecto_PWIII_RAZOR/DAO.Model/Comment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.Model
+{
+    public class Comment
+    {
+        public byte CommentID { get; set; }
+        public string Description { get; set; }
+        public string CommentType { get; set; }
+        public string StudentName { get; set; }
+
+        public Comment()
+        {
+
+        }
+        public Comment(byte commentID, string description, string commentType, string studentName)
+        {
+            CommentID = commentID;
+            Description = description;
+            CommentType = commentType;
+            StudentName = studentName;
+        }
+        public Comment(byte commentID)
+        {
+            CommentID = commentID;
+        }
+    }
+}
